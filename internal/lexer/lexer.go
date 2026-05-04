0b5407ae7f23cda5655122a57d12e2fd520c5b67
oshankkkk <oshankodagoda200667@gmail.com>
2026-05-05 00:11:47 +0530

    feat: improve lexer logic and add keyword support

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -15,7 +15,7 @@ func ReadFile(file *os.File){
 	for scanner.Scan() {
 		tokens:=lexer(scanner.Text())
 		tokenlist = append(tokenlist, tokens)
-	fmt.Println(scanner.Text())
+		fmt.Println(scanner.Text())
 	}
 	fmt.Println(tokenlist)
 }
@@ -52,106 +52,168 @@ const (
 	NOT_EQUAL     TokenType = "NOT_EQUAL"
 	LESS_EQUAL    TokenType = "LESS_EQUAL"
 	GREATER_EQUAL TokenType = "GREATER_EQUAL"
+	IDENTIFIER TokenType = "IDENTIFIER"
+	WHITESPACE TokenType ="WHITESPACE"
+
+	IF     TokenType = "IF"
+	ELSE   TokenType = "ELSE"
+	THEN   TokenType = "THEN"
+	END    TokenType = "END"
+	FUNC   TokenType = "FUNC"
+	LOCAL  TokenType = "LOCAL"
+	RETURN TokenType = "RETURN"
+
+	WHILE TokenType = "WHILE"
+	FOR   TokenType = "FOR"
+	BREAK TokenType = "BREAK"
+	CONTINUE TokenType = "CONTINUE"
+
+	TRUE  TokenType = "TRUE"
+	FALSE TokenType = "FALSE"
+	NIL   TokenType = "NIL"
+
 )
+//readIdentifier()
+//readNumber()
+//readString()
+//skipComment()
+//matchNextChar('=')
+
+
 
 //lexer was just sequentially going through the characters and categorizing them into groups every time it finds a break point (an invalid character, space, operator, etc).
 type Token struct{
 	ID int
 	Type TokenType 
 	Value string
-
 }
-
 
 func lexer(source string)[]Token{
-	
-var singleCharTokens = map[string]TokenType{
-	"(": LEFT_PAREN,
-	")": RIGHT_PAREN,
-	"{": LEFT_BRACE,
-	"}": RIGHT_BRACE,
-	"[": LEFT_BRACKET,
-	"]": RIGHT_BRACKET,
-	",": COMMA,
-	".": DOT,
-	";": SEMICOLON,
-	":": COLON,
 
-	"+": PLUS,
-	"-": MINUS,
-	"*": STAR,
-	"/": SLASH,
-	"%": PERCENT,
+	var singleCharTokens = map[string]TokenType{
+		"(": LEFT_PAREN,
+		")": RIGHT_PAREN,
+		"{": LEFT_BRACE,
+		"}": RIGHT_BRACE,
+		"[": LEFT_BRACKET,
+		"]": RIGHT_BRACKET,
+		",": COMMA,
+		".": DOT,
+		";": SEMICOLON,
+		":": COLON,
 
-	">": GREATER,
-	"<": LESS,
-	"!": NOT,
-	"=": EQUAL,
+		"+": PLUS,
+		"-": MINUS,
+		"*": STAR,
+		"/": SLASH,
+		"%": PERCENT,
 
-	"'":  SINGLE_QUOTES,
-	"\"": DOUBLE_QUOTES,
-}
+		">": GREATER,
+		"<": LESS,
+		"!": NOT,
+		"=": EQUAL,
 
+		" ": WHITESPACE,
+		"'":  SINGLE_QUOTES,
+		"\"": DOUBLE_QUOTES,
 
-var doubleCharTokens = map[string]TokenType{
-	"==": EQUAL_EQUAL,
-	"!=": NOT_EQUAL,
-	"<=": LESS_EQUAL,
-	">=": GREATER_EQUAL,
-}
-	var tokenlist []Token
-	doubletoken:=false
-	stringtoken:=false
-	for index,value:=range source{
-		character:=string(value)
-		if doubletoken{
-			newToken:=tokenlist[len(tokenlist)-1].Value+character
-			if tokenType,ok:=doubleCharTokens[newToken];ok{
-			tokenlist[len(tokenlist)-1].Type=tokenType	
-			tokenlist[len(tokenlist)-1].Value=newToken
-			continue
-			}
-		}
-		if stringtoken{
-
-		}
-		tokenType, ok := singleCharTokens[character] 
-		if ok {
-	if slices.Contains([]TokenType{SINGLE_QUOTES,DOUBLE_QUOTES},tokenType){
-				if !stringtoken{
-					stringtoken=true
-					tokenType = STRING
-			tokenlist = append(tokenlist, Token{
-				Type:  tokenType,
-				Value: character,
-				ID:    index,
-			})
-	
-				}else{stringtoken=false
-					continue
-			}
-			}
-			if !stringtoken{
-			if slices.Contains([]TokenType{GREATER,LESS,NOT,EQUAL},tokenType){
-				doubletoken=true
-			}
-		
-
-			tokenlist = append(tokenlist, Token{
-				Type:  tokenType,
-				Value: character,
-				ID:    index,
-			})
-	
-			
-		}
-	}	
-
-		// if equal sign then if 	
 	}
 
 
-return tokenlist
-}
+	var doubleCharTokens = map[string]TokenType{
+		"==": EQUAL_EQUAL,
+		"!=": NOT_EQUAL,
+		"<=": LESS_EQUAL,
+		">=": GREATER_EQUAL,
+
+	}
+	var keywordTokens=map[string]TokenType{
+		"if":       IF,
+		"else":     ELSE,
+		"then":     THEN,
+		"end":      END,
+		"func":     FUNC,
+		"local":    LOCAL,
+		"return":   RETURN,
+
+		"while":    WHILE,
+		"for":      FOR,
+		"break":    BREAK,
+		"continue": CONTINUE,
+
+		"true":     TRUE,
+		"false":    FALSE,
+		"nil":      NIL,
+
+	}
+	//Scope highlighting plugin
+	var tokenlist []Token
+	stringtoken:=false
+	newlist:=""
+	for index,value:=range source{
+		character:=string(value)
+		tokenType, singleCharToken:= singleCharTokens[character]
+
+		if !(singleCharToken){
+			newlist+=character
+			continue
+		}
+
+		newstringtoken:=newlist
+		if len(newlist)>1 && !stringtoken{
+
+			newstringtoken=newlist[:len(newlist)-1]}
+
+			//tokenising the seperated
+			if slices.Contains([]TokenType{SINGLE_QUOTES,DOUBLE_QUOTES},tokenType){
+				if !stringtoken{
+					stringtoken=true
+				}else{
+					stringtoken=false
+					tokenlist = append(tokenlist, Token{
+						Type: STRING,
+						Value: newstringtoken,
+						ID:    index,
+					})
+				}
+			}else
+			if tokenType,ok:=doubleCharTokens[ newstringtoken];ok{
+				tokenlist = append(tokenlist, Token{
+					Type:  tokenType,
+					Value: newlist,
+					ID:    index,
+				})
+
+
+			}else if tokenType,ok:=keywordTokens[newstringtoken]; ok{
+				tokenlist = append(tokenlist, Token{
+					Type:  tokenType,
+					Value: newlist,
+					ID:    index,
+				})
+
+			}else if stringtoken{
+				continue
+			}else {
+				tokenlist = append(tokenlist, Token{
+					Type: IDENTIFIER,
+					Value: newlist,
+					ID:    index,
+				})
+
+			}
+
+			//tokenising the seperator
+			tokenlist = append(tokenlist, Token{
+				Type:  tokenType,
+				Value: character,
+				ID:    index,
+			})
+
+			newlist=""
+		}
+
+		return tokenlist
+	}
 
 
